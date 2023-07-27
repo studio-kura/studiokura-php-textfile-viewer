93793e841c49b0cb476e5023f33e9b1bf7db82e0
Alejandro Cremades <ale@alecrem.com>
2023-07-27 16:12:26 +0900

    Protect index and view with a password and PHP session

--- view.php
+++ view.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
 if (!isset($_GET['s'])) die('No input');
 if (strlen($_GET['s']) < 5) die('No input');
 if (strpos($_GET['s'], '.txt') == false) die('No input');
 $config = include('config.php');
+if (!isset($_SESSION['token'])) {
+  header('Location: login.php');
+  exit();
+}
 $filename = $config['path'] . $_GET['s'];
 echo '<pre>';
 readfile($filename);
